488fad73a8dd393474b8f549299ca2ab3dd67adc
Sachin Waghamare <sachinhw27@gmail.com>
2023-01-15 23:24:31 +0530

    my first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+from math_calculation.multiplication import get_multiplication
+from math_calculation.division import get_division
+from math_calculation.addition import get_addition
+
+if __name__ == "__main__":
+    get_division(30,5)
+    get_multiplication(3,7)
+    get_addition(100,200)
