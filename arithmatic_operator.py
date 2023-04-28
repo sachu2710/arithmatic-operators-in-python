5db1581897cad86ea98cd960c1eb3794ce214ff1
Sachin Waghamare <sachinhw27@gmail.com>
2023-04-28 09:49:53 +0530

    arithmatic operators im python

---
+++ arithmatic_operator.py
@@ -0,0 +1,41 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a=10
+b=2
+print("a/b=",a/b)
+a/b= 5.0
+print("a%b",a%b)
+a%b 0
+print("a//b",a//b)
+a//b 5
+print("a//b=",a//b)
+a//b= 5
+print("a**b=",a**b)
+a**b= 100
+a=10.5
+b=2
+print("a/b=",a/b)
+a/b= 5.25
+print("a//b=",a//b)
+a//b= 5.0
+print("a/b",a/b)
+a/b 5.25
+a=10
+b=3
+print("a/b=",a/b)
+a/b= 3.3333333333333335
+print("a//b=",a//b)
+a//b= 3
+if(a>b):
+    print("a is greater than b")
+    else:
+        
+SyntaxError: invalid syntax
+
+print("a is greater than b")
+a is greater than b
+if(a<b):
+    
+KeyboardInterrupt
+print("a is greater than b")
+a is greater than b
